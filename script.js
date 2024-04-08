86f1357f3bce7f3a3da8f812d848c17a82f23594
Castoxity Sama <sameerrajchal@gmail.com>
2024-04-08 12:18:19 +0545

    html n table

---
+++ script.js
@@ -0,0 +1,8 @@
+function e(e){
+    var n,d;
+    if (e==0){
+    d="C:\Users\Samee\OneDrive\Desktop\Castoxity\memez\blood circulating.png"    }else{
+        d="C:\Users\Samee\OneDrive\Desktop\Castoxity\memez\-_-.jpg"
+    }
+    document.getElementById('n').src=d;
+        }
